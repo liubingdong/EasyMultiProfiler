04757ac6a4eb470d580c0583edf4b59ac8bc7adc
huerqiang <13766876214@163.com>
2024-04-13 06:12:21 +0800

    document

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' EMP_prepare_data
+#'
+#' A data.frame xx
+#'
+#' @format data.frame
+#'
+#' @return data.frame
+#' 
+"EMP_prepare_data"
+
+#' MAE
+#'
+#' A data.frame xx
+#'
+#' @format data.frame
+#'
+#' @return data.frame
+#' 
+"MAE"
