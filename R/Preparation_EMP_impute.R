98c12fa25bcb92c0e7bc1f61dbfc6dbd099fe426
liubingdong <382983280@qq.com>
2024-07-11 04:07:05 +0800

    add cached for EMP_impute

--- R/Preparation_EMP_impute.R
+++ R/Preparation_EMP_impute.R
@@ -1,47 +1,5 @@
-#' Fast Imputation of Missing Values by Chained Random Forests
-#'
-#' @param obj Object in EMPT or MultiAssayExperiment format.
-#' @param experiment A character string. Experiment name in the MultiAssayExperiment object.
-#' @param coldata A boolean. Whether the function use the coldata to impute or not.(default:TRUE)
-#' @param assay A boolean. Whether the function use the assay to impute or not.(default:FALSE)
-#' @param rowdata A boolean. Whether the function use the rowdata to impute or not.(default:FALSE)
-#' @param .formula A formula.A two-sided formula specifying variables to be imputed (left hand side) and variables used to impute (right hand side). Defaults to . ~ ., i.e., use all variables to impute all variables. For instance, if all variables (with missings) should be imputed by all variables except variable "ID", use . ~ . - ID. Note that a "." is evaluated separately for each side of the formula. Further note that variables with missings must appear in the left hand side if they should be used on the right hand side.
-#' @param pmm.k Number of candidate non-missing values to sample from in the predictive mean matching steps. 0 to avoid this step.
-#' @param num.trees Tree number.
-#' @param seed Integer seed to initialize the random generator.
-#' @param verbose Controls how much info is printed to screen. 0 to print nothing.
-#' @param action A character string. Whether to join the new information to the EMPT (add), or just get the detailed result generated here (get).
-#' @param ... Further parameters passed to the function missRanger::missRanger.
 #' @importFrom missRanger missRanger
-#'
-#' @return EMPT object
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' data(MAE)
-#' ## For coldata
-#' MAE |>
-#'   EMP_assay_extract('geno_ec') |>
-#'   EMP_impute(assay=FALSE,coldata=TRUE,rowdata=FALSE) |>
-#'   EMP_coldata_extract()
-#' ## Support formula, such as only impute SAS and SDS
-#' MAE |>
-#'   EMP_assay_extract('geno_ec') |>
-#'   EMP_impute(.formula = HAMA+HAMD ~ .) |>
-#'   EMP_coldata_extract()
-#' 
-#' ## For assay
-#' MAE |>
-#'   EMP_assay_extract('geno_ec') |>
-#'   EMP_impute(assay=TRUE,coldata=FALSE,rowdata=FALSE)
-#' 
-#' ## For rowdata (Not Not recommended)
-#' MAE |>
-#'   EMP_assay_extract('geno_ec') |>
-#'   EMP_impute(assay=FALSE,coldata=FALSE,rowdata=TRUE)
-#' }
-EMP_impute <- function(obj,experiment,coldata = TRUE,assay = FALSE, rowdata = FALSE,.formula=. ~ .,
+.EMP_impute <- function(obj,experiment,coldata = TRUE,assay = FALSE, rowdata = FALSE,.formula=. ~ .,
                        pmm.k = 10, num.trees = 1000, seed = 123,verbose = 0,action='add',...){
   call <- match.call()
   if (inherits(obj,"MultiAssayExperiment")) {
@@ -97,3 +55,69 @@ EMP_impute <- function(obj,experiment,coldata = TRUE,assay = FALSE, rowdata = FA
     stop("action should be one of add or get")
   }
 }
+
+#' @importFrom memoise memoise
+.EMP_impute_m <- memoise::memoise(.EMP_impute,cache = cachem::cache_mem(max_size = 2048 * 1024^2))
+
+#' Fast Imputation of Missing Values by Chained Random Forests
+#'
+#' @param obj Object in EMPT or MultiAssayExperiment format.
+#' @param experiment A character string. Experiment name in the MultiAssayExperiment object.
+#' @param coldata A boolean. Whether the function use the coldata to impute or not.(default:TRUE)
+#' @param assay A boolean. Whether the function use the assay to impute or not.(default:FALSE)
+#' @param rowdata A boolean. Whether the function use the rowdata to impute or not.(default:FALSE)
+#' @param .formula A formula.A two-sided formula specifying variables to be imputed (left hand side) and variables used to impute (right hand side). Defaults to . ~ ., i.e., use all variables to impute all variables. For instance, if all variables (with missings) should be imputed by all variables except variable "ID", use . ~ . - ID. Note that a "." is evaluated separately for each side of the formula. Further note that variables with missings must appear in the left hand side if they should be used on the right hand side.
+#' @param pmm.k Number of candidate non-missing values to sample from in the predictive mean matching steps. 0 to avoid this step.
+#' @param num.trees Tree number.
+#' @param seed Integer seed to initialize the random generator.
+#' @param verbose Controls how much info is printed to screen. 0 to print nothing.
+#' @param use_cached A boolean. Whether the function use the results in cache or re-compute.
+#' @param action A character string. Whether to join the new information to the EMPT (add), or just get the detailed result generated here (get).
+#' @param ... Further parameters passed to the function missRanger::missRanger.
+#' @importFrom missRanger missRanger
+#'
+#' @return EMPT object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data(MAE)
+#' ## For coldata
+#' MAE |>
+#'   EMP_assay_extract('geno_ec') |>
+#'   EMP_impute(assay=FALSE,coldata=TRUE,rowdata=FALSE) |>
+#'   EMP_coldata_extract()
+#' ## Support formula, such as only impute SAS and SDS
+#' MAE |>
+#'   EMP_assay_extract('geno_ec') |>
+#'   EMP_impute(.formula = HAMA+HAMD ~ .) |>
+#'   EMP_coldata_extract()
+#' 
+#' ## For assay
+#' MAE |>
+#'   EMP_assay_extract('geno_ec') |>
+#'   EMP_impute(assay=TRUE,coldata=FALSE,rowdata=FALSE)
+#' 
+#' ## For rowdata (Not Not recommended)
+#' MAE |>
+#'   EMP_assay_extract('geno_ec') |>
+#'   EMP_impute(assay=FALSE,coldata=FALSE,rowdata=TRUE)
+#' }
+EMP_impute <- function (obj,experiment=NULL,coldata = TRUE,assay = FALSE, rowdata = FALSE,.formula=. ~ .,
+                       pmm.k = 10, num.trees = 1000, seed = 123,verbose = 0,use_cached=TRUE,action='add',...) {
+  call <- match.call()
+  deposit <- list()
+
+  if (use_cached == FALSE) {
+    memoise::forget(.EMP_impute_m) %>% invisible()
+  }
+  
+  deposit <- .EMP_impute_m(obj=obj,experiment=experiment,coldata = coldata,assay = assay, rowdata = rowdata,.formula=.formula,
+                       pmm.k = pmm.k, num.trees = num.trees, seed = seed,verbose = verbose,action=action,...)
+  if (action=='add') {
+    .get.history.EMPT(deposit) <- call
+  }
+  return(deposit)
+}
+
+
