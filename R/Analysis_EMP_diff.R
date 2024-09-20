b25b5dfa975df1ac6fcc989314d9bea529a481a0
382983280@qq.com <382983280@qq.com>
2024-09-20 23:16:13 +0800

    increase feature threshold for diff

--- R/Analysis_EMP_diff.R
+++ R/Analysis_EMP_diff.R
@@ -141,7 +141,7 @@
 #' @param use_cached A boolean. Whether the function use the results in cache or re-compute.
 #' @param action A character string. Whether to join the new information to the EMPT (add), or just get the detailed result generated here (get).
 #' @param group_level A series of character strings. Determine the comparison order of groups.
-#' @param core A number. Select the core numbers in the parallel compute to speed up the result.
+#' @param core A number. Select the core numbers in the parallel compute to speed up the result.When no core is set and the number of features exceeds 2000, the default will use all CPU cores minus one.
 #' @param ... Further parameters passed to the function tidybulk::test_differential_abundance, or statistical function in the stats package.
 #' @importFrom memoise forget
 #'
@@ -252,7 +252,7 @@ EMP_diff_analysis <- function(obj,experiment,.formula,
 
   
   # When feature num is not many, core = 1 will be more efficient
-  if (is.null(core) & length(feature_name) < 500) {
+  if (is.null(core) & length(feature_name) < 2000) {
     core <- 1
   }
 
