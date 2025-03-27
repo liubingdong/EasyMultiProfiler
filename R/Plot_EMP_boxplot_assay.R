020e508063057860d86c875c61ca955b0730b021
382983280@qq.com <382983280@qq.com>
2025-03-28 07:12:50 +0800

    speed up and add paired for diff

--- R/Plot_EMP_boxplot_assay.R
+++ R/Plot_EMP_boxplot_assay.R
@@ -4,7 +4,7 @@
 #' @importFrom ggsignif geom_signif
 EMP_boxplot_assay_default <- function (EMPT,method = 'wilcox.test',
                                estimate_group = NULL,group_level = 'default',dot_size=2,box_width=NULL,box_alpha=1,
-                               step_increase=0.1,ref.group=NULL,comparisons = NULL,
+                               step_increase=0.1,ref.group=NULL,comparisons = NULL,paired_group=NULL,
                                ncol = NULL,show = 'pic',palette = NULL,
                                html_width=NULL,html_height=NULL,
                                mytheme = 'theme()',...) {
@@ -20,8 +20,9 @@ EMP_boxplot_assay_default <- function (EMPT,method = 'wilcox.test',
 
   EMPT %<>% .group_level_modified(estimate_group = estimate_group,
                                   group_level = group_level)
-
-  mapping <- .get.mapping.EMPT(EMPT) %>% dplyr::select(primary,!!estimate_group)
+  
+ 
+  mapping <- .get.mapping.EMPT(EMPT) %>% dplyr::select(primary,dplyr::any_of(c(!!estimate_group,!!paired_group)))
 
   data <-.get.result.EMPT(EMPT,info = 'EMP_assay_data') %>% suppressMessages() %>% dplyr::left_join(mapping,by ='primary') 
 
@@ -53,23 +54,55 @@ EMP_boxplot_assay_default <- function (EMPT,method = 'wilcox.test',
     comparisons <- comparisons
   }
 
-  data %<>%  tidyr::pivot_longer(cols = c(-primary,-!!dplyr::sym(estimate_group)),
+  data %<>%  tidyr::pivot_longer(cols = c(-primary,-dplyr::any_of(c(!!estimate_group,!!paired_group))),
                                        names_to = 'ID',
                                        values_to = 'value')
+  # For paired test, order is necessary!
+  if (is.null(paired_group)) {
+    data <- data
+  }else{
+    ## check the missing value in the group label
+    if(any(is.na(data[[paired_group]]))) {
+        stop('Column ',paired_group,' has beed deteced missing value, please check and filter them!')
+    }
+    ## check the paired sample and size
+    check_paired_result <- .check_group_consistency(data=data,group_col=estimate_group,patient_col=paired_group)
+    if(check_paired_result$all_patients_equal == FALSE) {
+      stop(check_paired_result$message)
+    }    
+    data <- data %>% dplyr::arrange(!!dplyr::sym(estimate_group),!!dplyr::sym(paired_group)) 
+  }
 
   data_plot <- list()
-  data_plot[['pic']] <- data %>%
+  if (is.null(paired_group)) {
+    data_plot[['pic']] <- data %>%
+      ggplot(., aes(x = !!dplyr::sym(estimate_group), y = value, fill = !!dplyr::sym(estimate_group))) +
+      geom_boxplot(outlier.color=NA,alpha=box_alpha,width=box_width) +
+      ggiraph::geom_jitter_interactive(aes(tooltip = paste0(primary,' : ',value)),shape=21,size=dot_size,position = position_jitter(height = .00000001))+
+      ggsignif::geom_signif(comparisons = comparisons,test = method,step_increase = step_increase,...) +
+      facet_wrap(ID~., scales = 'free', strip.position = 'top',ncol = ncol) +
+      xlab(NULL) + 
+      ggtitle('Feature Boxplot') +
+      scale_fill_manual(values = col_values) +
+      theme_bw() + 
+      theme(axis.text.x =element_text(angle = 45, hjust = 1,size = 10)) +
+      eval(parse(text = paste0(mytheme)))
+  }else{
+    data_plot[['pic']] <- data %>%
     ggplot(., aes(x = !!dplyr::sym(estimate_group), y = value, fill = !!dplyr::sym(estimate_group))) +
-    geom_boxplot(outlier.color=NA,alpha=box_alpha,width=box_width) +
-    ggiraph::geom_jitter_interactive(aes(tooltip = paste0(primary,' : ',value)),shape=21,size=dot_size,position = position_jitter(height = .00000001))+
-    ggsignif::geom_signif(comparisons = comparisons,test = method,step_increase = step_increase,...) +
-    facet_wrap(ID~., scales = 'free', strip.position = 'top',ncol = ncol) +
-    xlab(NULL) + 
-    ggtitle('Feature Boxplot') +
-    scale_fill_manual(values = col_values) +
-    theme_bw() + 
-    theme(axis.text.x =element_text(angle = 45, hjust = 1,size = 10)) +
-    eval(parse(text = paste0(mytheme)))
+      geom_boxplot(outlier.color=NA,alpha=box_alpha,width=box_width) +
+      ggiraph::geom_jitter_interactive(aes(tooltip = paste0(primary,' : ',value)),shape=21,size=dot_size,position = position_jitter(width = 0))+
+      ggsignif::geom_signif(comparisons = comparisons,test = method,test.args=list(paired=TRUE),step_increase = step_increase,...) +
+      geom_line(aes(group = !!dplyr::sym(paired_group)), color = 'gray', lwd = 0.5) + 
+      facet_wrap(ID~., scales = 'free', strip.position = 'top',ncol = ncol) +
+      xlab(NULL) + 
+      ggtitle('Feature Boxplot') +
+      scale_fill_manual(values = col_values) +
+      theme_bw() + 
+      theme(axis.text.x =element_text(angle = 45, hjust = 1,size = 10)) +
+      eval(parse(text = paste0(mytheme)))
+  }
+ 
 
 
   data_plot[['html']] <- ggiraph::girafe(code = print(data_plot[['pic']]),width = html_width,height = html_height)
@@ -87,7 +120,7 @@ EMP_boxplot_assay_default <- function (EMPT,method = 'wilcox.test',
 
 EMP_boxplot_assay_violin  <- function (EMPT,method = 'wilcox.test',
                                estimate_group = NULL,group_level = 'default',dot_size=2,box_width=0.3,box_alpha=0.8,
-                               step_increase=0.1,ref.group=NULL,comparisons = NULL,
+                               step_increase=0.1,ref.group=NULL,comparisons = NULL,paired_group=NULL,
                                ncol = NULL,show = 'pic',palette = NULL,
                                html_width=NULL,html_height=NULL,
                                mytheme = 'theme()',...) {
@@ -104,7 +137,7 @@ EMP_boxplot_assay_violin  <- function (EMPT,method = 'wilcox.test',
   EMPT %<>% .group_level_modified(estimate_group = estimate_group,
                                   group_level = group_level)
 
-  mapping <- .get.mapping.EMPT(EMPT) %>% dplyr::select(primary,!!estimate_group)
+  mapping <- .get.mapping.EMPT(EMPT) %>% dplyr::select(primary,dplyr::any_of(c(!!estimate_group,!!paired_group)))
 
   data <-.get.result.EMPT(EMPT,info = 'EMP_assay_data') %>% suppressMessages() %>% dplyr::left_join(mapping,by ='primary') 
 
@@ -136,25 +169,58 @@ EMP_boxplot_assay_violin  <- function (EMPT,method = 'wilcox.test',
     comparisons <- comparisons
   }
 
-  data %<>%  tidyr::pivot_longer(cols = c(-primary,-!!dplyr::sym(estimate_group)),
+
+  data %<>%  tidyr::pivot_longer(cols = c(-primary,-dplyr::any_of(c(!!estimate_group,!!paired_group))),
                                        names_to = 'ID',
                                        values_to = 'value')
+  
+  # For paired test, order is necessary!
+  if (is.null(paired_group)) {
+    data <- data
+  }else{
+    ## check the missing value in the group label
+    if(any(is.na(data[[paired_group]]))) {
+        stop('Column ',paired_group,' has beed deteced missing value, please check and filter them!')
+    }
+    ## check the paired sample and size
+    check_paired_result <- .check_group_consistency(data=data,group_col=estimate_group,patient_col=paired_group)
+    if(check_paired_result$all_patients_equal == FALSE) {
+      stop(check_paired_result$message)
+    }       
+    data <- data %>% dplyr::arrange(!!dplyr::sym(estimate_group),!!dplyr::sym(paired_group)) 
+  }
 
   data_plot <- list()
-  data_plot[['pic']] <- data %>%
-    ggplot(., aes(x = !!dplyr::sym(estimate_group), y = value, fill = !!dplyr::sym(estimate_group))) +
-    geom_violin(position = position_dodge(width = 0.1), scale = 'width',alpha=box_alpha) +
-    geom_boxplot(outlier.color=NA,fill="white", width=box_width) +
-    ggiraph::geom_jitter_interactive(aes(tooltip = paste0(primary,' : ',value)),shape=21,size=dot_size,position = position_jitter(height = .00000001))+
-    ggsignif::geom_signif(comparisons = comparisons,test = method,step_increase = step_increase,...) +
-    facet_wrap(ID~., scales = 'free', strip.position = 'top',ncol = ncol) +
-    xlab(NULL) + 
-    ggtitle('Feature Boxplot') +
-    scale_fill_manual(values = col_values) +
-    theme_bw() + 
-    theme(axis.text.x =element_text(angle = 45, hjust = 1,size = 10)) +
-    eval(parse(text = paste0(mytheme)))
-
+  if (is.null(paired_group)) {
+    data_plot[['pic']] <- data %>%
+      ggplot(., aes(x = !!dplyr::sym(estimate_group), y = value, fill = !!dplyr::sym(estimate_group))) +
+      geom_violin(position = position_dodge(width = 0.1), scale = 'width',alpha=box_alpha) +
+      geom_boxplot(outlier.color=NA,fill="white", width=box_width) +
+      ggiraph::geom_jitter_interactive(aes(tooltip = paste0(primary,' : ',value)),shape=21,size=dot_size,position = position_jitter(height = .00000001))+
+      ggsignif::geom_signif(comparisons = comparisons,test = method,step_increase = step_increase,...) +
+      facet_wrap(ID~., scales = 'free', strip.position = 'top',ncol = ncol) +
+      xlab(NULL) + 
+      ggtitle('Feature Boxplot') +
+      scale_fill_manual(values = col_values) +
+      theme_bw() + 
+      theme(axis.text.x =element_text(angle = 45, hjust = 1,size = 10)) +
+      eval(parse(text = paste0(mytheme)))
+  }else{
+    data_plot[['pic']] <- data %>%
+      ggplot(., aes(x = !!dplyr::sym(estimate_group), y = value, fill = !!dplyr::sym(estimate_group))) +
+      geom_violin(position = position_dodge(width = 0.1), scale = 'width',alpha=box_alpha) +
+      geom_boxplot(outlier.color=NA,fill="white", width=box_width) +
+      ggiraph::geom_jitter_interactive(aes(tooltip = paste0(primary,' : ',value)),shape=21,size=dot_size,position = position_jitter(width = 0))+
+      ggsignif::geom_signif(comparisons = comparisons,test = method,test.args=list(paired=TRUE),step_increase = step_increase,...) +
+      geom_line(aes(group = !!dplyr::sym(paired_group)), color = 'gray', lwd = 0.5) + 
+      facet_wrap(ID~., scales = 'free', strip.position = 'top',ncol = ncol) +
+      xlab(NULL) + 
+      ggtitle('Feature Boxplot') +
+      scale_fill_manual(values = col_values) +
+      theme_bw() + 
+      theme(axis.text.x =element_text(angle = 45, hjust = 1,size = 10)) +
+      eval(parse(text = paste0(mytheme)))
+  }
 
   data_plot[['html']] <- ggiraph::girafe(code = print(data_plot[['pic']]),width = html_width,height = html_height)
 
@@ -166,14 +232,6 @@ EMP_boxplot_assay_violin  <- function (EMPT,method = 'wilcox.test',
   class(EMPT) <- 'EMP_assay_boxplot'
   EMPT
 }
-
-
-
-
-
-
-
-
 
 .show_EMP_assay_boxplot <- function(obj,plot) {
   result <- .get.plot_deposit.EMPT(obj,info = 'EMP_assay_boxplot')
@@ -196,6 +254,7 @@ EMP_boxplot_assay_violin  <- function (EMPT,method = 'wilcox.test',
 #' @param ref.group a character string specifying the reference group. If specified, for a given grouping variable, each of the group levels will be compared to the reference group (i.e. control group).
 #' @param comparisons A list of length-2 vectors. The entries in the vector are either the names of 2 values on the x-axis or the 2 integers that correspond to the index of the columns of interest.(default:NULL)
 #' @param ncol An interger. Set the col number in the facet plot.
+#' @param paired_group  A character string. Variable name corresponding to paired primary or sample.
 #' @param select_metrics A series of character string. Select the alpha metrics to show. 
 #' @param show A character string include pic (default), html.
 #' @param palette A series of character string. Color palette.
@@ -208,7 +267,7 @@ EMP_boxplot_assay_violin  <- function (EMPT,method = 'wilcox.test',
 EMP_boxplot.EMP_assay_boxplot_union <- function(obj,plot_category = 1,method = 'wilcox.test',
                                estimate_group = NULL,group_level = 'default',
                                dot_size=2,box_width=if (plot_category == "violin") 0.3 else NULL,box_alpha=if (plot_category == "violin") 0.8 else 1,
-                               step_increase=0.1,ref.group=NULL,comparisons = NULL,
+                               step_increase=0.1,ref.group=NULL,comparisons = NULL,paired_group=NULL,
                                ncol = NULL,show = 'pic',palette = NULL,
                                html_width=NULL,html_height=NULL,
                                mytheme = 'theme()',...) {
@@ -220,7 +279,7 @@ EMP_boxplot.EMP_assay_boxplot_union <- function(obj,plot_category = 1,method = '
            withr::with_seed(123,EMP_boxplot_assay_default(EMPT=obj,method = method,
                                estimate_group = estimate_group,group_level = group_level,
                                dot_size=dot_size,box_width=box_width,box_alpha=box_alpha,
-                               step_increase = step_increase,ref.group = ref.group,comparisons = comparisons,
+                               step_increase = step_increase,ref.group = ref.group,comparisons = comparisons,paired_group=paired_group,
                                ncol = ncol,show = show,palette = palette,
                                html_width=html_width,html_height=html_height,
                                mytheme = mytheme,...))
@@ -229,7 +288,7 @@ EMP_boxplot.EMP_assay_boxplot_union <- function(obj,plot_category = 1,method = '
            withr::with_seed(123,EMP_boxplot_assay_violin(EMPT=obj,method = method,
                                estimate_group = estimate_group,group_level = group_level,
                                dot_size=dot_size,box_width=box_width,box_alpha=box_alpha,
-                               step_increase = step_increase,ref.group = ref.group,comparisons = comparisons,
+                               step_increase = step_increase,ref.group = ref.group,comparisons = comparisons,paired_group=paired_group,
                                ncol = ncol,show = show,palette = palette,
                                html_width=html_width,html_height=html_height,
                                mytheme = mytheme,...))
